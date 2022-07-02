7b56470cae710250fdd707ac28d2e4ffcea529a1
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2022-07-03 00:57:21 +0200

    Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<template>
+  <div class="bg-dark-back h-[100vh] w-[100vw] p-4 lg:pt-10 text-dark-main-font flex flex-col items-center">
+    <header class="w-full max-w-[1024px]">
+      <h1 class="text-2xl font-poppins font-bold lg:text-3xl">Apprendre le japonais</h1>
+      <div class="flex items-center">
+        <h2 class="text-2xl mt-1 ml-3 mr-2 font-notojp lg:text-3xl">日本語の学習</h2>
+        <div class="h-[0.125rem] bg-dark-accent flex-grow"/>
+      </div>
+    </header>
+    
+    <div class="flex flex-col text-center flex-grow justify-evenly items-center my-10 w-[80%] max-w-[1024px]">
+      <RouterLink to="/" class="text-4xl lg:text-5xl font-poppins font-bold bg-dark-secondary-back p-10 rounded-[1.25rem] max-w-[700px] w-full">Kanji</RouterLink>
+      <RouterLink to="/" class="text-4xl lg:text-5xl font-poppins font-bold bg-dark-secondary-back p-10 rounded-[1.25rem] max-w-[700px] w-full">Exressions</RouterLink>
+      <RouterLink to="/" class="text-4xl lg:text-5xl font-poppins font-bold bg-dark-secondary-back p-10 rounded-[1.25rem] max-w-[700px] w-full">Custom</RouterLink>
+    </div>
+    
+  </div>
+  <RouterView />
+</template>
+
+<script>
+  import { RouterLink, RouterView } from 'vue-router'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        notojp: ["Noto Sans JP", 'sans-serif']
+      }
+    },
+    colors:{
+      'dark-back': '#282626',
+      'dark-secondary-back': '#313131',
+      'dark-main-font': '#e0e0e0',
+      'dark-secondary-font': "#464646",
+      'dark-accent': '#ff643f',
+      'light-back': '#f2f2f2',
+      'light-secondary-back': '#fdfdfd',
+      'light-main-font': '#464443',
+      'light-secondary-font': '#d6d6d6',
+      'light-accent': '#ff643f'
+    }
   },
   plugins: [],
 }
